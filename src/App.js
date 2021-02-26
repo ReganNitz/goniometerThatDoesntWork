54a306dedabd588bf8b9555c16d1ae9c9ccf3177
mrfisher-oe <michael.fisher@orbiseducation.com>
2021-02-26 10:52:28 -0500

    minor updates

--- src/App.js
+++ src/App.js
@@ -1,47 +1,40 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      clicks: 45,
-      show: true,
-      rotate: 45
-    };
-  }
+function App() {
 
-  IncrementItem = () => {
-    this.setState({ clicks: this.state.clicks + 1 });
-    this.setState({ rotate: this.state.rotate + 1 });
-  }
-  DecreaseItem = () => {
-    this.setState({ clicks: this.state.clicks - 1 });
-    this.setState({ rotate: this.state.rotate - 1 });
-  }
-  ToggleClick = () => {
-    this.setState({ show: !this.state.show });
-  }
+  const [show, setShow] = useState(true);
+  const [rotateAngle, setRotateAngle] = useState(45);
 
 
-  render() {
+  const changeAngle = (value) => {
+
+    setRotateAngle(rotateAngle + value);
+
+  };
+
+
+  const toggleClick = () => {
+    setShow(!show);
+  };
+
+  
     return (
       <div>
-        <button className="math" onClick={this.IncrementItem}>Add</button>
-        <button className="math" onClick={this.DecreaseItem}>Subtract</button>
-        <button className="math" onClick={this.ToggleClick}>
-          { this.state.show ? "Hide number" : "Show number" }
+        <button className="math" onClick={(event) => {changeAngle(1); }}>Add</button>
+        <button className="math" onClick={(event) => {changeAngle(-1); }}>Subtract</button>
+        <button className="math" onClick={toggleClick}>
+          { show ? "Hide number" : "Show number" }
         </button>
-        { this.state.show ? <h2>{ this.state.clicks }</h2> : null }
+        { show ? <h2>{ rotateAngle }</h2> : null }
         <div className="circle"> 
           <div className="verticalLine">
           </div>
-          <div className="movingLine" style={{transform: "rotate(" + this.state.rotate + "deg)"}}>
+          <div className="movingLine" style={{transform: "rotate(" + rotateAngle + "deg)"}}>
             <div className="movingLineHandle"></div>
           </div>
         </div>
       </div>
     );
-  }
-}
+};
 
 export default App;
